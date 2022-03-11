a41279488192cbf8b41d20d3cd88f10679bc3922
Niravcodez <84302842+Niravcodez@users.noreply.github.com>
2022-03-10 18:04:48 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 status = "";
+objects = [];
 
 function setup()
 {
@@ -13,6 +14,41 @@ function setup()
 function draw()
 {
     image(video, 0, 0, 380, 380);
+
+    if(status != "")
+    {
+        objectDetector.detect(video, gotResults);
+
+        for(i = 0; i < objects.length; i++)
+        {
+            document.getElementById("status").innerHTML = "Status : Objects Detected";
+    
+            fill("red");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+            noFill();
+            stroke("red");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+
+            if(objects[i].label == object_name)
+            {
+                video.stop();
+                objectDetector.detect(gotResults);
+                document.getElementById("object_status").innerHTML = object_name + " found";
+                synth = window.speechSynthesis;
+                utterThis = new SpeechSynthesisUtterance(object_name + "found");
+                synth.speak(utterThis);
+            }
+            else
+            {
+                document.getElementById("object_status").innerHTML = object_name + " not found";
+                synth = window.speechSynthesis;
+                utterThis = new SpeechSynthesisUtterance(object_name + "not found");
+                synth.speak(utterThis);
+                s
+            }
+        }
+    }
 }
 
 function start()
@@ -26,4 +62,14 @@ function modelLoaded()
 {
     console.log("MODEL IS LOADED!!");
     status = true;
+}
+
+function gotResults(error, results)
+{
+    if(error)
+    {
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
 }
